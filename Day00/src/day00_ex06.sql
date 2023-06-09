b0abe228febcaaefbe5288c46079878ea9533f46
dejaneom <dejaneom@student.21-school.ru>
2023-06-09 22:05:12 +0300

    day00

---
+++ Day00/src/day00_ex06.sql
@@ -0,0 +1,13 @@
+select
+	(select name
+	 from person
+	 where person.id = po.person_id) as name,
+	 case
+	 	when (select name
+	 		  from person 
+	 		  where person.id = po.person_id) = 'Denis' then true 
+	 	else false 
+	 	end as check_name	 
+from person_order as po
+where (menu_id = 13 or menu_id = 14 or menu_id = 18) 
+	and order_date = '2022-01-07';
